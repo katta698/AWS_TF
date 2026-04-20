23cab7485f4d36ed3678973fd82ed526961f7244
katta698 <katta.jayant@gmail.com>
2026-04-19 22:55:59 -0500

    new file

--- main.tf
+++ main.tf
@@ -1,72 +1,34 @@
-# Configure AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "my-vpc"
-  }
+# Create IAM user
+resource "aws_iam_user" "demo_user" {
+  name = "terraform-demo-user"
 }
 
-# Create a Subnet
-resource "aws_subnet" "main_subnet" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+# Create policy
+resource "aws_iam_policy" "demo_policy" {
+  name        = "terraform-demo-policy"
+  description = "Simple read-only policy"
 
-  tags = {
-    Name = "my-subnet"
-  }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:Describe*",
+          "s3:List*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
-# Create a Security Group
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP and SSH"
-  vpc_id      = aws_vpc.main_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # SSH (be careful in real world)
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # HTTP
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web-sg"
-  }
-}
-
-# Create an EC2 Instance
-resource "aws_instance" "web_server" {
-  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 (check region)
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.main_subnet.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  tags = {
-    Name = "my-ec2-instance"
-  }
-}
-
-output "public_ip" {
-  value = aws_instance.web_server.public_ip
+# Attach policy to user
+resource "aws_iam_user_policy_attachment" "attach" {
+  user       = aws_iam_user.demo_user.name
+  policy_arn = aws_iam_policy.demo_policy.arn
 }
